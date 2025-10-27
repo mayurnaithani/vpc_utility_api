7ea0ef5770c0af64ff1e849c9bf130fa692e5fe1
mayurnaithani <mayur.naithani@hotmail.com>
2025-10-27 12:02:43 +0530

    Update variables for APIGW module

--- infra/terraform/modules/apigw/variables.tf
+++ infra/terraform/modules/apigw/variables.tf
@@ -1,9 +1,8 @@
 variable "api_name_in" {}
 variable "cognito_user_pool_id_in" {}
-variable "var.vpc_automation_sqs_in" {}
 variable "get_vpc_lambda_arn_in" {}
-variable "var.aws_region_in" {}
-variable "var.aws_account_in" {}
-variable "var.sqs_queue_in" {}
+variable "aws_region_in" {}
+variable "aws_account_in" {}
+variable "sqs_queue_in" {}
 variable "lambda_in" {}
 variable "sqs_role_apigw_in" {}
