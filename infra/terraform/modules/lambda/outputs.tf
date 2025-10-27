3988717b3dcb164443575fc934f9579aa6cdce88
mayurnaithani <mayur.naithani@hotmail.com>
2025-10-27 12:08:40 +0530

    Update outputs section of lambda module

--- infra/terraform/modules/lambda/outputs.tf
+++ infra/terraform/modules/lambda/outputs.tf
@@ -1,3 +1,7 @@
 output "get_vpc_lambda_arn" {
   value    = aws_lambda_function.vpc_lambda['get'].arn
 }
+
+output "get_vpc_lambda_name" {
+  value    = aws_lambda_function.vpc_lambda['get'].name
+}
